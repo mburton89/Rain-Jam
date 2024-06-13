4ba3ce4612a9d9ae2eda17ba40241ebe423a31ee
WUStudent <wustudent@webster.edu>
2024-06-13 09:48:11 -0500

    yes

---
+++ Rain-Jam-Unity/Assets/Code/WaterSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSpawner : MonoBehaviour
+{
+    public static WaterSpawner Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
