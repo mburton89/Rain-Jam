e78acae302d8145ede1ed6bcc81280fd799ddbc8
Bommar257 <j2bomm@gmail.com>
2024-06-13 10:43:08 -0500

    attempt 1

--- Rain-Jam-Unity/Assets/Code/ObstacleSpawner.cs
+++ Rain-Jam-Unity/Assets/Code/ObstacleSpawner.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
+    //Variables
+    public int timeEnd;
+    public int randSpawnMin = 1;
+    public int randSpawnMax = 10;
+    public GameObject platform;
+    public Rigidbody2D rg2D;
+
+    public Transform target; // the target position
+    
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,31 @@ public class ObstacleSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Time.deltaTime < timeEnd)
+        {
+            ObstacleSpawn();
+        }
+       
         
     }
+
+    public void ObstacleSpawn()
+    {
+        
+  
+            int platformNumberSpwaned =  Random.Range(randSpawnMin, randSpawnMax);
+            for(int i = 0; i < platformNumberSpwaned; i++)
+            {
+                Instantiate(platform);
+            }
+           if (Time.time == 0 && platform.transform.position == target.position)
+            {
+                for(int i = 0;i < platformNumberSpwaned; i++)
+                {
+                    Destroy(platform);
+                }
+            }
+    }
+
+
 }
