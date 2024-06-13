6ba73f70e087b07c7954a94c2588f0f91d24a49b
Bommar257 <j2bomm@gmail.com>
2024-06-13 11:23:48 -0500

    ahaha

--- Rain-Jam-Unity/Assets/Code/ObstacleSpawner.cs
+++ Rain-Jam-Unity/Assets/Code/ObstacleSpawner.cs
@@ -19,31 +19,33 @@ public class ObstacleSpawner : MonoBehaviour
     public float speed; // speed - units per second (gives you control of how fast the object will move in the inspector)
     public bool moveObj; // a public bool that allows you to toggle this script on and off in the inspector
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (moveObj == true)
-        {
-            float step = speed * Time.deltaTime; // step size = speed * frame time
-            transform.position = Vector3.MoveTowards(transform.position, target.position, step); // moves position a step closer to the target position
-        }
-
-    }
-
 
     void Start()
     {
         StartCoroutine(Spawn());
     }
 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
     IEnumerator Spawn()
     {
         while (true)
         {
             Instantiate(obj[Random.Range(0, obj.Length)], pos, Quaternion.identity);
+            
+            float step = speed * Time.deltaTime; // step size = speed * frame time
+            obj[0].transform.position = Vector3.MoveTowards(transform.position, target.position, step); // moves position a step closer to the target position
+          
             pos += dir * size;
             yield return new WaitForSeconds(Random.Range(spawnMin, spawnMax));
-                Destroy(target);
         }
     }
 
